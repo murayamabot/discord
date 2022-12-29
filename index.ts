638b1e1e3d836c0d83dc0698d442ccb3b888212d
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2022-12-29 17:40:27 +0100

    chore: update sern to 2.0

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { Client, GatewayIntentBits } from 'discord.js';
-import { Sern, SernEmitter } from '@sern/handler';
+import { DefaultLogging, Dependencies, Sern, SernEmitter, single, Singleton } from '@sern/handler';
 import 'dotenv/config'
 
 const client = new Client({
@@ -8,12 +8,22 @@ const client = new Client({
 		GatewayIntentBits.GuildMembers,
 	],
 });
-//View docs for all options
+
+interface MyDependencies extends Dependencies {
+    '@sern/client' : Singleton<Client>;
+    '@sern/logger' : Singleton<DefaultLogging>
+}
+export const useContainer = Sern.makeDependencies<MyDependencies>({
+    build: root => root
+        .add({ '@sern/client': single(client)  }) 
+        .add({ '@sern/logger': single(new DefaultLogging()) })
+});
 Sern.init({
-	client,
 	commands: 'dist/commands',
 	events: 'dist/events',
-	sernEmitter: new SernEmitter()
+	containerConfig: {
+		get: useContainer
+	}
 });
 
 client.login(process.env.TOKEN);
